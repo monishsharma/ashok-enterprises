49dbd564637d2547246938e1358ec2238dce9f76
monishsharma <sharma.monish17@gmail.com>
2024-06-10 17:20:33 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Allow requests from http://localhost:5173
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://ashok-enterprises-fe.vercel.app',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
