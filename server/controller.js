393fd274414257088a4e282cc4d5e0866dd3f9ed
Monish sharma <sharma.monish17@gmail.com>
2024-06-11 16:32:04 +0530

    Update controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,32 @@
-export const cron = () => {
+import {db} from "./db.connection.js"
+
+export const cron = async() => {
     
       //code for the automated task
+    try {
+
+        // Access the collection
+        const collection = db.collection('employeeDetails');
+        const today = new Date();
+        const tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+
+        // Construct the attendance object for the next day
+        const attendanceObj = {
+            date: tomorrow.toISOString().split('T')[0],
+            status: false,
+            isSunday: tomorrow.getDay() === 0, // Check if tomorrow is Sunday
+            checkinTime: "",
+            checkoutTime: "",
+            isOverTime: false
+        };
+
+        // Update attendance for all employees
+        await collection.updateMany({}, { $push: { attendance: attendanceObj } });
+
+        console.log('Attendance updated successfully for all employees.');
+    } catch (err) {
+        console.error('Error updating attendance:', err);
+    }
 
 };
