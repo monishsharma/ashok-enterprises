02beffc439b24df84a3a7d4a69c87c7389863b16
monishsharma <sharma.monish17@gmail.com>
2024-06-27 14:09:01 +0530

    changeS

--- server/db/connection.js
+++ server/db/connection.js
@@ -15,6 +15,7 @@ try {
   await client.connect();
   // Send a ping to confirm a successful connection
   await client.db("admin").command({ ping: 1 });
+  console.log(process.env.NODE_ENV)
   console.log(
    "Pinged your deployment. You successfully connected to MongoDB!"
   );
