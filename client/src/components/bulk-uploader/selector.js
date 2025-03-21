a2df64cdbc1ac461ff9c71b2874a8600a3323b0d
monishsharma <sharma.monish17@gmail.com>
2025-03-21 16:46:07 +0530

    fix in checkout bulk

--- client/src/components/bulk-uploader/selector.js
+++ client/src/components/bulk-uploader/selector.js
@@ -3,10 +3,10 @@ export const getEmployee = (data) => {
     const employee = [];
     data.filter(emp=> {
         if (emp.attendance && emp.attendance.length) {
-            const detail = emp.attendance[0];
-            if (!detail.status && !detail.isAbsent) {
+            // const detail = emp.attendance[0];
+            // if (!detail.status && !detail.isAbsent) {
                 employee.push(emp);
-            }
+            // }
         } else {
             employee.push(emp);
         }
