0a922978fb83581662c98ff2927aba4135da9814
monishsharma <sharma.monish17@gmail.com>
2025-05-08 17:20:31 +0530

    updated railway.json for cron jobs

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,8 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const connection_url = "mongodb+srv://sharmamonish17:DX2YvfTRFgPrxcKV@cluster-ae.viv07hp.mongodb.net/AEDB?retryWrites=true&w=majority&appName=Cluster-AE";
-import { MongoClient, ServerApiVersion } from "mongodb";
-
-const client = new MongoClient(connection_url, {
+const uri = connection_url || "";
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -11,19 +10,18 @@ const client = new MongoClient(connection_url, {
   },
 });
 
-let db;
-
-export async function connectToDatabase() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("✅ Connected to MongoDB");
-    db = client.db("AEDB");
-    return db;
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-    process.exit(1); // Exit so Railway sees failure
-  }
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(process.env.NODE_ENV)
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
 }
 
-export { db };
+let db = client.db("AEDB");
+export default db;
