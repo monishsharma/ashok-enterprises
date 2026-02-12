bb34f9395983fae85d20ad0904d0e3f216d0ea99
monishsharma <sharma.monish17@gmail.com>
2026-02-12 16:08:10 +0530

    minor fix

--- client/src/pages/dashboard/component.jsx
+++ client/src/pages/dashboard/component.jsx
@@ -1,19 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Calender from "../../shared/component/calendar";
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { getMonth } from "../../helpers/today-date";
 import "./style.css";
 import moment from "moment";
+import DatePicker from "react-datepicker";
 
 const Dashboard = ({ data, employeeListConnect }) => {
+  const getDateValue = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth(); // January is 0, December is 11
+    const day = today.getDate();
+
+    let salaryMonth, salaryYear;
+
+    if (day <= 10) {
+      // Before or on the 10th, return the previous month
+      if (month === 0) {
+        // If current month is January, go to December of the previous year
+        salaryMonth = 11; // December
+        salaryYear = year - 1;
+      } else {
+        salaryMonth = month - 1;
+        salaryYear = year;
+      }
+    } else {
+      // After the 10th, return the current month
+      salaryMonth = month;
+      salaryYear = year;
+    }
+
+    // Return a Date object with the calculated month and year
+    return new Date(salaryYear, salaryMonth);
+  };
+
+  const [dateValue, setDateValue] = useState(new Date(getDateValue()));
   const [isLoading, setIsLoading] = useState(false);
 
   const employeeListHandler = () => {
     setIsLoading(true);
     const qp = {
-      month: getMonth(),
-      year: new Date().getFullYear(),
+      month: getMonth(dateValue),
+      year: new Date(dateValue).getFullYear(),
     };
     employeeListConnect({ sortByKey: "empCode", qp })
       .then(() => {
@@ -26,14 +56,43 @@ const Dashboard = ({ data, employeeListConnect }) => {
 
   useEffect(() => {
     employeeListHandler();
-  }, []);
+  }, [dateValue]);
+
+  const handleDateChange = (selectedDate) => {
+    setDateValue(selectedDate);
+  };
+
+  // eslint-disable-next-line react/display-name
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <div className="d-flex ">
+      <Button
+        variant="dark"
+        className="example-custom-input"
+        onClick={onClick}
+        ref={ref}
+      >
+        {value}
+      </Button>
+    </div>
+  ));
 
   return (
     <React.Fragment>
-      <h2 className="fw-bold mt-4 mb-4">
-        Dashboard -{" "}
-        <span className="fw-normal"> {moment().format("MMMM, YY")}</span>
-      </h2>
+      <Row className="gy-2">
+        <Col sm={3}>
+          <h2 className="fw-bold mt-4 mb-4">Dashboard</h2>
+        </Col>
+      </Row>
+      <DatePicker
+        selected={dateValue}
+        showMonthYearPicker={true}
+        dateFormat="MMM, yyyy"
+        onChange={handleDateChange}
+        withPortal
+        customInput={<ExampleCustomInput />}
+        // filterDate={isSelectableDate}
+      />
+
       <div className="dashboardContainer">
         <div className="colContainer">
           <div className="calendarCard">
--- client/src/shared/component/calendar/calender-days.jsx
+++ client/src/shared/component/calendar/calender-days.jsx
@@ -1,78 +1,112 @@
+import moment from "moment";
 import { getMonth, month } from "../../../helpers/today-date";
 import "./style.css";
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
 
 const CalendarDays = (props) => {
+  let firstDayOfMonth = new Date(
+    props.day.getFullYear(),
+    props.day.getMonth(),
+    1,
+  );
+  let weekdayOfFirstDay = firstDayOfMonth.getDay();
+  let currentDays = [];
 
-    let firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
-    let weekdayOfFirstDay = firstDayOfMonth.getDay();
-    let currentDays = [];
+  for (let day = 0; day < 42; day++) {
+    if (day === 0 && weekdayOfFirstDay === 0) {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+    } else if (day === 0) {
+      firstDayOfMonth.setDate(
+        firstDayOfMonth.getDate() + (day - weekdayOfFirstDay),
+      );
+    } else {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+    }
 
-    for (let day = 0; day < 42; day++) {
-        if (day === 0 && weekdayOfFirstDay === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
-        } else if (day === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
-        } else {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+    let calendarDay = {
+      currentMonth: firstDayOfMonth.getMonth() === props.day.getMonth(),
+      date: new Date(firstDayOfMonth),
+      month: firstDayOfMonth.getMonth(),
+      number: firstDayOfMonth.getDate(),
+      selected: firstDayOfMonth.toDateString() === props.day.toDateString(),
+      year: firstDayOfMonth.getFullYear(),
+      // monthName: getMonth(firstDayOfMonth.getMonth()),
+    };
+
+    currentDays.push(calendarDay);
+  }
+
+  const isPresent = (day) => {
+    const filteredData = props.employee.attendance.filter(
+      (emp) =>
+        emp.month &&
+        month[day.month] &&
+        parseInt(new Date(emp.date).getDate()) === parseInt(day.number),
+    );
+    if (filteredData && filteredData[0]) {
+      if (filteredData[0].checkinTime) {
+        if (filteredData[0].checkoutTime) {
+          return "present";
         }
-
-        let calendarDay = {
-          currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
-          date: (new Date(firstDayOfMonth)),
-          month: firstDayOfMonth.getMonth(),
-          number: firstDayOfMonth.getDate(),
-          selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
-          year: firstDayOfMonth.getFullYear()
-        }
-
-        currentDays.push(calendarDay);
+        return "checkoutNotMarked";
       }
+      return "absent";
+    } else {
+      return "N/A";
+    }
+  };
 
-      const isPresent = (day) => {
-        const filteredData = props.employee.attendance.filter((emp) => emp.month &&  month[day.month] &&  parseInt(new Date(emp.date).getDate()) === parseInt(day.number))
-        if (filteredData && filteredData[0]) {
-          if (filteredData[0].checkinTime) {
-            if (filteredData[0].checkoutTime) {
-              return "present";
-            }
-            return "checkoutNotMarked"
-          }
-          return "absent"
-        } else {
-          return "N/A"
-        }
+  const getStyle = (day) => {
+    console.log(day);
+    const result = isPresent(day);
+    if (result === "N/A") return "";
+    if (result === "present") return "present";
+    if (result === "absent") return "absent";
+    if (result === "checkoutNotMarked") return "checkoutNotMarked";
+  };
+
+  const formatTime = (time) => {
+    const formattedTime = moment(parseInt(time)).format("hh:mm: a");
+    return time ? formattedTime : "N/A";
+  };
+
+  const getTitle = (day) => {
+    let title = "";
+    props.employee.attendance.forEach((emp) => {
+      if (
+        emp.month === month[day.month] &&
+        parseInt(new Date(emp.date).getDate()) === parseInt(day.number)
+      ) {
+        title = `${formatTime(emp.checkinTime)} - ${formatTime(emp.checkoutTime)}`;
       }
+    });
+    return title;
+  };
 
-      const getStyle = (day) => {
-        const result = isPresent(day);
-        if (result === "N/A") return "";
-        if (result === "present") return "present";
-        if (result === "absent") return "absent";
-        if (result === "checkoutNotMarked") return "checkoutNotMarked";
-      }
-
-      return (
-
-        <div className="customCalendar">
-            {
-                currentDays.map((day) => {
-                    return (
-                        <div key={day} className={` ${day.currentMonth ? " current" : "disabled"}  ${day.selected ? " selected" : ""}`}>
-                            <p className={`${day.currentMonth ? getStyle(day) : ""}`}>{day.number}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  return (
+    <div className="customCalendar">
+      {currentDays.map((day) => {
+        return (
+          <div
+            key={day}
+            className={` ${day.currentMonth ? " current" : "disabled"}  ${day.selected ? " selected" : ""}`}
+          >
+            <p
+              title={getTitle(day)}
+              className={`${day.currentMonth ? getStyle(day) : ""}`}
+            >
+              {day.number}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 CalendarDays.propTypes = {
-    day: PropTypes.any,
-    employee: PropTypes.object
-}
+  day: PropTypes.any,
+  employee: PropTypes.object,
+};
 
-
-export default CalendarDays
+export default CalendarDays;
