6f0b3e7528fc5f25f65a76133b68ecbaed7323f6
monish <sharma.monish17@gmail.com>
2026-06-11 18:21:27 +0530

    Downgraded firbase/admin

--- server/config/env.js
+++ server/config/env.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV || "development"}`
-});
+if (!process.env.VERCEL) {
+  dotenv.config({
+    path: `.env.${process.env.NODE_ENV || "development"}`,
+  });
+}
